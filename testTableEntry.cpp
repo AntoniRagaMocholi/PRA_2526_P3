4cb24a64e1f7bcc4a2fba5875dc684d32eaf2cbd
AntoniRagaMocholi <antoniwintermono@gmail.com>
2025-11-25 20:08:07 +0100

    Modificado testTableEntry.cpp

--- testTableEntry.cpp
+++ testTableEntry.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include "TableEntry.h"
-using namespace std;
 
-int main() {
+int main(){
+    std::cout << std::boolalpha; // configuramos cout para mostrar true/false en lugar de 0/1.
+
     TableEntry<int> e1("Catorze", 14);
     TableEntry<int> e2("Trenta-tres", 33);
     TableEntry<int> e3("Trenta-tres", 123);
 
-    cout << "e1: " << e1 << endl;
-    cout << "e2: " << e2 << endl;
-    cout << "e3: " << e3 << endl << endl;
-
-    cout << "e1 == e2 ? " << (e1 == e2 ? "true" : "false") << endl;
-    cout << "e1 != e2 ? " << (e1 != e2 ? "true" : "false") << endl << endl;
-
-    cout << "e1 == e3 ? " << (e1 == e3 ? "true" : "false") << endl;
-    cout << "e1 != e3 ? " << (e1 != e3 ? "true" : "false") << endl << endl;
-
-    cout << "e2 == e3 ? " << (e2 == e3 ? "true" : "false") << endl;
-    cout << "e2 != e3 ? " << (e2 != e3 ? "true" : "false") << endl;
-
+    std::cout << "e1: " << e1 << std::endl;
+    std::cout << "e2: " << e2 << std::endl;
+    std::cout << "e3: " << e3 << std::endl;
+    
+    std::cout << std::endl;
+    
+    std::cout << "e1 == e2 ? " << (e1 == e2) << std::endl;
+    std::cout << "e1 != e2 ? " << (e1 != e2) << std::endl;
+    std::cout << std::endl;
+    std::cout << "e1 == e3 ? " << (e1 == e3) << std::endl;
+    std::cout << "e1 != e3 ? " << (e1 != e3) << std::endl;
+    std::cout << std::endl;
+    std::cout << "e2 == e3 ? " << (e2 == e3) << std::endl;
+    std::cout << "e2 != e3 ? " << (e2 != e3) << std::endl;
     return 0;
 }
-
