8785a175a4b827939fede0bf3014da7c35fd88f8
AntoniRagaMocholi <antoniwintermono@gmail.com>
2025-11-27 16:02:05 +0100

    Solucionados errores de acceso a métodos y ajustada la clase HashTable

--- HashTable.h
+++ HashTable.h
@@ -5,96 +5,98 @@
 #include <stdexcept>
 #include "Dict.h"
 #include "TableEntry.h"
-
 #include "../PRA_2526_P1/ListLinked.h"
 
 template <typename V>
 class HashTable : public Dict<V> {
-
 private:
     int n;
     int max;
     ListLinked<TableEntry<V>>* table;
-
     int h(std::string key) {
-        int suma = 0;
-        for (int i = 0; i < key.size(); i++)
-            suma += int(key.at(i));
-        return suma % max;
+        int sum = 0;
+        for (char c : key) {
+            sum += static_cast<int>(c);
+        }
+        return sum % max;
     }
 
 public:
     HashTable(int size) {
-        n = 0;
         max = size;
+        n = 0;
         table = new ListLinked<TableEntry<V>>[max];
     }
-
     ~HashTable() {
         delete[] table;
     }
-
     int capacity() {
         return max;
     }
 
-    void insert(std::string key, V value) override {
-        int pos = h(key);
-        TableEntry<V> temp(key);
-
-        if (table[pos].contains(temp))
-            throw std::runtime_error("Key '" + key + "' already exists!");
-
-        table[pos].append(TableEntry<V>(key, value));
-        n++;
-    }
-
-    V search(std::string key) override {
-        int pos = h(key);
-        TableEntry<V> temp(key);
-
-        int index = table[pos].indexOf(temp);
-        if (index == -1)
-            throw std::runtime_error("Key '" + key + "' not found!");
-
-        return table[pos].get(index).value;
-    }
-
-    V remove(std::string key) override {
-        int pos = h(key);
-        TableEntry<V> temp(key);
-
-        int index = table[pos].indexOf(temp);
-        if (index == -1)
-            throw std::runtime_error("Key '" + key + "' not found!");
-
-        V val = table[pos].get(index).value;
-        table[pos].remove(index);
-        n--;
-        return val;
-    }
-
-    int entries() override {
-        return n;
-    }
-
+   
     V operator[](std::string key) {
         return search(key);
     }
 
-    friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &ht) {
-        out << "HashTable [entries: " << ht.n
-            << ", capacity: " << ht.max << "]\n==============\n\n";
+    
+    void insert(std::string key, V value) override {
+        int pos = h(key);
+        TableEntry<V> temp(key, value);
 
-        for (int i = 0; i < ht.max; i++) {
-            out << "== Cubeta " << i << " ==\n\n";
-            out << "List => " << ht.table[i] << "\n\n";
+        if (table[pos].contains(temp)) {
+            throw std::runtime_error("Key already exists");
         }
-
-        out << "==============\n";
-        return out;
+        table[pos].insertAtEnd(temp);
+        n++;
     }
+
+    
+    V search(std::string key) override {
+        int pos = h(key);
+        TableEntry<V> temp(key);
+
+        if (table[pos].contains(temp)) {
+            return table[pos].indexOf(temp).getValue();
+        }
+        throw std::runtime_error("Key not found");
+    }
+
+    
+    V remove(std::string key) override {
+        int pos = h(key);
+        TableEntry<V> temp(key);
+
+        if (table[pos].contains(temp)) {
+            V value = table[pos].indexOf(temp).getValue();
+            table[pos].remove(temp);
+            n--;
+            return value;
+        }
+        throw std::runtime_error("Key not found");
+    }
+
+   
+    int entries() override {
+        return n;
+    }
+
+    
+    friend std::ostream& operator<<(std::ostream& out, const HashTable<V>& ht) {
+    	out << "HashTable [entries: " << ht.n << ", capacity: " << ht.max << "]\n";
+    	out << "==============\n";
+    	for (int i = 0; i < ht.max; ++i) {
+        	out << "== Cubeta " << i << " ==\n";
+        	out << "List => [";
+        	for (auto entry = ht.table[i].begin(); entry != ht.table[i].end(); ++entry) {
+            		out << " ('" << entry->data.getKey() << "' => " << entry->data.getValue() << ")";
+        }
+  		out << "]\n";
+    	}
+    	out << "==============\n";
+   	return out;
+    }
+
 };
 
 #endif
-
--- TableEntry.h
+++ TableEntry.h
@@ -2,31 +2,47 @@
 #define TABLEENTRY_H
 
 #include <string>
-#include <ostream>
 
 template <typename V>
 class TableEntry {
-public:
+private:
     std::string key;
     V value;
 
+public:
+    
     TableEntry(std::string key, V value) : key(key), value(value) {}
-    TableEntry(std::string key) : key(key) {}
-    TableEntry() : key("") {}
 
+    TableEntry(std::string key) : key(key), value(V()) {}
+
+  
+    TableEntry() : key(""), value(V()) {}
+
+   
+    std::string getKey() const {
+        return key;
+    }
+
+   
+    V getValue() const {
+        return value;
+    }
+
+    
     friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
         return te1.key == te2.key;
     }
 
+   
     friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
         return te1.key != te2.key;
     }
 
-    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
-        out << "('" << te.key << "' => " << te.value << ")";
+   
+    friend std::ostream& operator<<(std::ostream& out, const TableEntry<V>& te) {
+        out << "(" << te.key << " => " << te.value << ")";
         return out;
     }
 };
 
 #endif
-
