016ec1a7a2127205e34ea85b2e0084c90ab3cec0
AntoniRagaMocholi <antoniwintermono@gmail.com>
2025-12-16 21:36:39 +0100

    Update TableEntry.h

--- TableEntry.h
+++ TableEntry.h
@@ -9,22 +9,22 @@ using namespace std;
 template <typename V> 
 class TableEntry{
     public:
-    //atributos
+
     string key;
     V value;
 
-    //constructores
+
     TableEntry(string key, V value) : key(key), value(value) {}
     TableEntry(string key) : key(key), value(V{}) {}
     TableEntry() : key(""), value(V{}) {}
 
-    //sobrecargda de ops
+
     friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
         return te1.key == te2.key;
     }
 
     friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
-        return !(te1 == te2); // Basada en la implementación de ==
+        return !(te1 == te2);
     }
 
     friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
